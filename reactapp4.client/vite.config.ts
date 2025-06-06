971a711ed2cbe2615611fe0936d6642cca705a61
Jack Petersen <jackpetersenmusic@gmail.com>
2025-06-06 11:29:56 -0700

    frontend

--- reactapp4.client/vite.config.ts
+++ reactapp4.client/vite.config.ts
@@ -1,70 +1,101 @@
+//import { fileURLToPath, URL } from 'node:url';
+//
+//import { defineConfig } from 'vite';
+//import plugin from '@vitejs/plugin-react';
+//import fs from 'fs';
+//import path from 'path';
+//import child_process from 'child_process';
+//
+//const baseFolder =
+//  process.env.APPDATA !== undefined && process.env.APPDATA !== ''
+//    ? path.join(process.env.APPDATA, 'ASP.NET', 'https') // Use path.join() for consistent path format
+//    : '/app';
+//
+////const baseFolder = '/app/.aspnet/https';
+//
+//console.log(baseFolder);
+//console.log(process.env.HOME);
+//console.log(process.env.APPDATA);
+//
+//const certificateArg = process.argv.map(arg => arg.match(/--name=(?<value>.+)/i)).filter(Boolean)[0];
+//const certificateName = certificateArg ? certificateArg.groups.value : "reactapp4.client";
+//
+//
+//if (!certificateName) {
+//    console.error('Invalid certificate name. Run this script in the context of an npm/yarn script or pass --name=<<app>> explicitly.')
+//    process.exit(-1);
+//}
+//
+//const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
+//const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
+//console.log(certFilePath)
+//console.log(keyFilePath)
+//
+//if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
+//    if (0 !== child_process.spawnSync('dotnet', [
+//        'dev-certs',
+//        'https',
+//        '--export-path',
+//        certFilePath,
+//        '--format',
+//        'Pem',
+//        '--no-password',
+//    ], { stdio: 'inherit', }).status) {
+//        throw new Error("Could not create certificate.");
+//    }
+//}
+//
+//// https://vitejs.dev/config/
+//export default defineConfig({
+//    plugins: [plugin()],
+//    resolve: {
+//        alias: {
+//            '@': fileURLToPath(new URL('./src', import.meta.url))
+//        }
+//    },
+//    server: {
+//        proxy: {
+//            
+//            '/api': {
+//                target: 'https://knucklehead-backend.onrender.com',
+//                secure: false
+//            }
+//        },
+//        port: 5173,
+//        https: {
+//            key: fs.readFileSync(keyFilePath),
+//            cert: fs.readFileSync(certFilePath),
+//        }
+//    }
+//})
+//
 import { fileURLToPath, URL } from 'node:url';
+import { defineConfig, UserConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-import { defineConfig } from 'vite';
-import plugin from '@vitejs/plugin-react';
-import fs from 'fs';
-import path from 'path';
-import child_process from 'child_process';
+const isDev = process.env.NODE_ENV !== 'production';
 
-const baseFolder =
-  process.env.APPDATA !== undefined && process.env.APPDATA !== ''
-    ? path.join(process.env.APPDATA, 'ASP.NET', 'https') // Use path.join() for consistent path format
-    : '/app';
-
-//const baseFolder = '/app/.aspnet/https';
-
-console.log(baseFolder);
-console.log(process.env.HOME);
-console.log(process.env.APPDATA);
-
-const certificateArg = process.argv.map(arg => arg.match(/--name=(?<value>.+)/i)).filter(Boolean)[0];
-const certificateName = certificateArg ? certificateArg.groups.value : "reactapp4.client";
-
-
-if (!certificateName) {
-    console.error('Invalid certificate name. Run this script in the context of an npm/yarn script or pass --name=<<app>> explicitly.')
-    process.exit(-1);
-}
-
-const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
-const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
-console.log(certFilePath)
-console.log(keyFilePath)
-
-if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-    if (0 !== child_process.spawnSync('dotnet', [
-        'dev-certs',
-        'https',
-        '--export-path',
-        certFilePath,
-        '--format',
-        'Pem',
-        '--no-password',
-    ], { stdio: 'inherit', }).status) {
-        throw new Error("Could not create certificate.");
-    }
-}
-
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [plugin()],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
+const config: UserConfig = {
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-    server: {
-        proxy: {
-            
-            '/api': {
-                target: 'https://knucklehead-backend.onrender.com',
-                secure: false
-            }
-        },
-        port: 5173,
-        https: {
-            key: fs.readFileSync(keyFilePath),
-            cert: fs.readFileSync(certFilePath),
-        }
-    }
-})
+  },
+};
+
+if (isDev) {
+  config.server = {
+    port: 5173,
+    //https: false, // set true if you're using certs locally
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5190',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  };
+}
+
+export default defineConfig(config);
