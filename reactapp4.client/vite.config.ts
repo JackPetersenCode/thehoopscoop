d0bc6692167d9a7c6d42fc7fcb4c814fe6d94290
Jack Petersen <jackpetersenmusic@gmail.com>
2025-06-06 12:04:50 -0700

    added backend url to vite config

--- reactapp4.client/vite.config.ts
+++ reactapp4.client/vite.config.ts
@@ -84,7 +84,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5190',
+        target: 'https://knucklehead-backend.onrender.com',
         changeOrigin: true,
         secure: false,
       },
